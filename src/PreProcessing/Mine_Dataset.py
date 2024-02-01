f7babbd19380b4cf8d52d495900a3148c40400f7
Farhangi <mohammadmeh.farhangi@didsr-wk46-021.fda.gov>
2024-02-01 14:03:00 -0500

    Preprocessing completed

--- src/PreProcessing/Mine_Dataset.py
+++ src/PreProcessing/Mine_Dataset.py
@@ -1,9 +1,9 @@
 import numpy as np
 import os
+import files_directories as f_dirs
 
 def mine_folder(dir_add):
     files = sorted(os.listdir(dir_add))
-    print(files[:10])
     patients = [x[:6] for x in files]   # number of patients in the dataset
     print('Number of patients in this folder is: ' + str(len(set(patients))))
     studies = [x[:9] for x in files]    # number of studies in the dataset; Each patient often underwent multiple CT examinations (studies) for different purposes or follow-up. i.e., this could be different followups
@@ -12,4 +12,4 @@ def mine_folder(dir_add):
     print('Number of series in this folder is: ' + str(len(set(series))))
 
 if __name__ == "__main__":
-    mine_folder(dir_add = '/gpfs_projects/mohammadmeh.farhangi/DiskStation/DeepLesion/NIH/nifti_dataset')
+    mine_folder(dir_add = f_dirs.nifti_files_head_add)
